ad82594081a1d4b577c9a76df53765beb1b6b634
Lenovo <Lenovo@DESKTOP-685NL8S>
2023-06-03 18:32:43 +0530

    This is my first commit...

---
+++ src/githubdemov1/Test1.java
@@ -0,0 +1,17 @@
+package githubdemov1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= 10,b=20;
+		
+		int t=a;
+		a=b;
+		b=t;
+		
+		System.out.println("print swap of two values" + a +" "+ b);
+	}
+
+}
